0d0e11b8035844570eb0e45a881049546ce718eb
shinsya <shinsya_bp@163.com>
2024-05-17 20:31:46 +0800

    complete service loop

--- src/client.rs
+++ src/client.rs
@@ -30,7 +30,10 @@ struct Args {
     ddns_name: Option<String>,
     /// ReDdns Record secret
     #[arg(short('s'), long("secret"))]
-    ddns_password: Option<String>
+    ddns_password: Option<String>,
+    /// Posting package interval (secs)
+    #[arg(short, long, default_value_t = 60)]
+    interval: u32
 }
 
 #[derive(Debug, Deserialize)]
@@ -38,11 +41,13 @@ struct Config {
     host: String,
     port: u16,
     name: String,
-    secret: String
+    secret: String,
+    interval: u32
 }
 
 struct Notifier {
     service_addr: SocketAddr,
+    interval: u32,
     pass: String,
     name: String
 }
@@ -70,6 +75,13 @@ fn main() {
         args.ddns_password = Some(config.secret);
         args.service_host = Some(config.host);
         args.service_port = config.port;
+        args.interval = config.interval;
+    }
+
+    // Interval min limit
+    if args.interval < 10 {
+        log::error!("Posting interval secs too small!");
+        exit(-1)
     }
 
     let mut notifier = Notifier::new(args).map_err(|msg| {
@@ -92,7 +104,7 @@ impl Notifier {
         // Timestamp
         let current = Utc::now().timestamp() as u64;
         // let current = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs();
-        println!("Current: {current}");
+        //println!("Current: {current}");
         buf[16..24].copy_from_slice(&current.to_le_bytes());
         // Name len
         buf[24..26].copy_from_slice(&(name.len() as u16).to_le_bytes());
@@ -118,6 +130,7 @@ impl Notifier {
 
         Ok(Self {
             service_addr: addr,
+            interval: arg.interval,
             pass: arg.ddns_password.ok_or("ReDdns record password not provided")?,
             name: arg.ddns_name.ok_or("ReDdns record name not provided")?
         })
@@ -132,46 +145,50 @@ impl Notifier {
                 exit(-1)
             }).unwrap();
         
-        // Send notify pack
-        let pack = Self::gen_pack(&self.pass, &self.name);
-        socket.send_to(&pack, self.service_addr).await.map_err(|e| {
-            log::error!("Sent notify pack to [{}] failure: {}", self.service_addr, e);
-            exit(-1)
-        }).unwrap();
+        loop {
+            // Send notify pack
+            let pack = Self::gen_pack(&self.pass, &self.name);
+            socket.send_to(&pack, self.service_addr).await.map_err(|e| {
+                log::error!("Sent notify pack to [{}] failure: {}", self.service_addr, e);
+                exit(-1)
+            }).unwrap();
 
-        // Wait response
-        let mut buf = [0u8; 32];
-        let code = match timeout(Duration::from_secs(30), Self::recv_from_target(&socket, self.service_addr, &mut buf)).await {
-            Ok(Ok(len)) => {
-                if len != 8 {
-                    log::error!("Received ReDdns response length is unexpected");
+            // Wait response
+            let mut buf = [0u8; 32];
+            let code = match timeout(Duration::from_secs(30), Self::recv_from_target(&socket, self.service_addr, &mut buf)).await {
+                Ok(Ok(len)) => {
+                    if len != 8 {
+                        log::error!("Received ReDdns response length is unexpected");
+                        exit(-1)
+                    }
+                    u64::from_le_bytes(buf[..8].try_into().unwrap())
+                },
+                Ok(Err(e)) => {
+                    log::error!("Receiving ReDdns response error: {e}");
+                    exit(-1)
+                },
+                Err(_) => {
+                    log::error!("Waiting ReDdns response timeout");
                     exit(-1)
                 }
-                u64::from_le_bytes(buf[..8].try_into().unwrap())
-            },
-            Ok(Err(e)) => {
-                log::error!("Receiving ReDdns response error: {e}");
-                exit(-1)
-            },
-            Err(_) => {
-                log::error!("Waiting ReDdns response timeout");
-                exit(-1)
-            }
-        };
+            };
 
-        // Code case
-        let msg = match code {
-            RESULT_OK => "OK",
-            RESULT_EXPIRED => "Local timestamp diff with remote service too large",
-            RESULT_INCORRECTED_PASS => "Incorrect ReDdns Record password",
-            RESULT_INVALID_NAME => "Invalid ReDdns Record name",
-            RESULT_INVALID_NAME_LEN => "Invalid ReDdns Record name length",
-            _ => {
-                log::error!("Unexpected response: [{code}]");
-                exit(-1)
-            }
-        };
-        log::info!("ReDdns Response: {msg}")
+            // Code case
+            let msg = match code {
+                RESULT_OK => "OK",
+                RESULT_EXPIRED => "Local timestamp diff with remote service too large",
+                RESULT_INCORRECTED_PASS => "Incorrect ReDdns Record password",
+                RESULT_INVALID_NAME => "Invalid ReDdns Record name",
+                RESULT_INVALID_NAME_LEN => "Invalid ReDdns Record name length",
+                _ => {
+                    log::error!("Unexpected response: [{code}]");
+                    exit(-1)
+                }
+            };
+            log::info!("ReDdns Response: {msg}");
+
+            tokio::time::sleep(Duration::from_secs(self.interval as u64)).await;
+        }
     }
 
     async fn recv_from_target(udp: &UdpSocket, target_addr: SocketAddr, buf: &mut[u8]) -> Result<usize, std::io::Error> {
